4a8243178e4ac1fb488b215286ab1c19e41f3731
Alexandre Peyroux <alex@xn--wxa.email>
2018-08-01 12:55:04 +0200

    init emacs

--- config.nix
+++ config.nix
@@ -4,5 +4,7 @@ with import <nixpkgs> {};
   allowUnfree = true;
   packageOverrides = pkgs: rec {
     vscode = (import ./vscode.nix { inherit pkgs; });
+    emacs = (import ./emacs.nix { inherit pkgs; });
+    flycheck-grammalecte = (import ./flycheck-grammalecte.nix { inherit pkgs; });
   };
 }
---
+++ emacs.nix
@@ -0,0 +1,74 @@
+{pkgs}:
+
+with pkgs;
+
+let
+
+  apps = with haskell.packages.ghc822; [
+      (import ./hie.nix { inherit pkgs; })
+      ag
+      aspellWithDictFR
+      binutils
+      cabal-install
+      cabal2nix
+      docker-machine
+      emacs-all-the-icons-fonts
+      gcc
+      ghc
+      git
+      gnumake
+      go
+      happy
+      hindent
+      hlint
+      hoogle
+      hpack
+      htmlTidy
+      isync
+      libxml2
+      makeWrapper
+      mercurial
+      multimarkdown
+      ncurses
+      present
+      python27Packages.rope
+      python36
+      python36Packages.autopep8
+      python36Packages.elpy
+      python36Packages.epc
+      python36Packages.flake8
+      python36Packages.grammalecte
+      python36Packages.importmagic
+      python36Packages.ipython
+      python36Packages.jedi
+      python36Packages.virtualenv
+      stack
+      structured-haskell-mode
+      stylish-haskell
+      tshark
+      w3m
+      xclip
+      xz
+      zip
+      zlib
+    ];
+
+  aspellWithDictFR = aspellWithDicts (ps: with ps; [ en fr ]);
+
+in
+
+  emacs.overrideDerivation (old: rec {
+    # wrapProgram $out/bin/emacs --set MU4E ${mu}
+    postInstall = with python34Packages; (old.postInstall + ''
+      wrapProgram $out/bin/emacs --prefix PATH : ${lib.makeBinPath apps}
+      wrapProgram $out/bin/emacs \
+            --prefix PYTHONPATH : "$(toPythonPath ${python3}):$(toPythonPath ${pip}):$PYTHONPATH" \
+            --prefix PYTHONPATH : "$(toPythonPath ${virtualenv})" \
+            --prefix PYTHONPATH : "$(toPythonPath ${elpy})" \
+            --prefix PYTHONPATH : "$(toPythonPath ${jedi})" \
+            --prefix PYTHONPATH : "$(toPythonPath ${autopep8})" \
+            --prefix PYTHONPATH : "$(toPythonPath ${flake8})" \
+	          --prefix PYTHONPATH : "$(toPythonPath ${grammalecte})" \
+	          --prefix PYTHONPATH : "$(toPythonPath ${python27Packages.rope})";
+      '');
+  })
