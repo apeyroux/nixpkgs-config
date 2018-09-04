de46b994cb68d02bfb62a0c57317f1255ebfcd66
Alexandre Peyroux <alex@xn--wxa.email>
2018-09-04 09:34:58 +0200

    up hie

--- hie.nix
+++ hie.nix
@@ -2,9 +2,11 @@
 
 with pkgs;
 
-(import (fetchFromGitHub {
-                owner="domenkozar";
-                repo="hie-nix";
-                rev="e3113da";
-                sha256="05rkzjvzywsg66iafm84xgjlkf27yfbagrdcb8sc9fd59hrzyiqk";
-            }) {}).hie84
+(import (fetchFromGitHub { owner = "apeyroux";
+                           repo = "hie-nix";
+                           rev = "update";
+                           sha256 = "03w21cn5n3z0ldjmnl2mzza9jysw5avhip6cpgb4anbjyvkzpnqf"; }) {}).hie84.overrideDerivation(old: {
+
+                           
+
+})
