2eff8d12d2d11a8bf82cde228d9a582100fa7b57
Alexandre Peyroux <alex@xn--wxa.email>
2018-09-04 09:35:14 +0200

    up protonmail-bridge

--- pm-bridge.nix
+++ pm-bridge.nix
@@ -2,7 +2,7 @@
 
 with pkgs;
 
-protonmail-bridge.overrideDerivation (old: {
+(import (fetchTarball https://github.com/NixOS/nixpkgs/archive/master.tar.gz) {}).protonmail-bridge.overrideDerivation (old: {
   installPhase = old.installPhase + ''
     ln -s $out/bin/Desktop-Bridge $out/bin/protonmail-bridge
   '';
