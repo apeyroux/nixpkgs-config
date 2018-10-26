36a4d74869bd40ef0975350c44f4b9a04478bdb9
Alexandre Peyroux <alex@px.io>
2018-10-26 19:55:59 +0200

    rm kitty and pm-bridge

--- config.nix
+++ config.nix
@@ -3,10 +3,8 @@ with import <nixpkgs> {};
 {
   allowUnfree = true;
   packageOverrides = pkgs: rec {
-    vscode = (import ./vscode.nix { inherit pkgs; });
+    # vscode = (import ./vscode.nix { inherit pkgs; });
     emacs = (import ./emacs.nix { inherit pkgs; });
-    kitty = (import ./kitty.nix { inherit pkgs; });
-    pm-bridge = (import ./pm-bridge.nix { inherit pkgs; });
     st = (import ./st.nix { inherit pkgs; });
   };
 }
