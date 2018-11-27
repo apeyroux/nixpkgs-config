6736685c04eaf0c61fcd391d54d221d4651db784
Alexandre Peyroux <alex@xn--wxa.email>
2018-11-27 18:15:17 +0100

    fix sha hie

--- hie.nix
+++ hie.nix
@@ -5,6 +5,6 @@ with pkgs;
 (import (fetchFromGitHub { owner = "domenkozar";
                            repo = "hie-nix";
                            rev = "master";
-                           sha256 = "1wjg9qal4d0hjb5p7rf8v7gr0z59fkzipxas65fd5xflzs4g9ama"; }) {}).hie84.overrideDerivation(old: {
+                           sha256 = "0hilxgmh5aaxg37cbdwixwnnripvjqxbvi8cjzqrk7rpfafv352q"; }) {}).hie84.overrideDerivation(old: {
                            
                            })
